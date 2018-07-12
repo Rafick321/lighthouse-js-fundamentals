9374e4c585573a21315c78c5b281cdd6cfcae811
Rafick321 <ralpha19@hotmail.com>
2018-07-12 16:55:57 +0000

    Adds looping exercises

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,4 @@
-var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "cups", "cooking utensils"];
+var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (var i = 0; i < packingList.length; i++) {
